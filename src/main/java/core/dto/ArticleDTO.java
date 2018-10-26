1c159947c81c1d073284585117f090455f40ff88
KozakMariia <kozak.mariia1@gmail.com>
2018-10-26 17:16:15 +0300

    Initial commit

---
+++ src/main/java/core/dto/ArticleDTO.java
@@ -0,0 +1,26 @@
+package core.dto;
+
+public class ArticleDTO {
+
+    private String headline;
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+
+
+}
---
+++ src/main/java/core/dto/ResultDTO.java
@@ -0,0 +1,19 @@
+package core.dto;
+
+
+import java.util.List;
+
+public class ResultDTO {
+
+    List<ArticleDTO> result;
+
+    public List<ArticleDTO> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ArticleDTO> result) {
+        this.result = result;
+    }
+
+
+}
